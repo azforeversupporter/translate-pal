fc42e9b10f00dcee24401c9cd68e4ecf74ab3864
azforeversupporter <azforeversupporter@gmail.com>
2016-05-01 17:25:53 +0200

    Added ApplicationDbContext

---
+++ src/TranslatePal.Data.SqlServer/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Data.Entity;
+
+namespace TranslatePal.Data.SqlServer
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
