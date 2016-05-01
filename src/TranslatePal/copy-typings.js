59707467fde1e1fb8509b3b82cef53febfe43539
azforeversupporter <azforeversupporter@gmail.com>
2016-05-01 21:21:44 +0200

    Added required development items

---
+++ src/TranslatePal/copy-typings.js
@@ -0,0 +1,8 @@
+﻿var mk = require('safe-mkdir');
+var gc = require('glob-copy');
+
+mk.mkdir('./typings/modules');
+gc('./node_modules/aurelia-*/dist/system/*.d.ts', './typings/modules', function (err, files) {
+
+    console.log('Copied ' + files.length + ' files to ./typings/modules');
+});
---
+++ src/TranslatePal/webpack.config.js
@@ -0,0 +1,40 @@
+﻿/* eslint-disable no-var */
+var path = require('path');
+var AureliaWebpackPlugin = require('aurelia-webpack-plugin');
+var ProvidePlugin = require('webpack/lib/ProvidePlugin');
+
+module.exports = {
+    resolve: {
+        extensions: ['', '.js', '.ts']
+    },
+    devServer: {
+        host: 'localhost',
+        port: 3000
+    },
+    entry: {
+        main: [
+            './client/main'
+        ]
+    },
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new AureliaWebpackPlugin(),
+        new ProvidePlugin({
+            Promise: 'bluebird'
+        })
+    ],
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts-loader' },
+            { test: /\.css?$/, loader: 'style!css' },
+            { test: /\.html$/, loader: 'raw' },
+            { test: /\.(png|gif|jpg)$/, loader: 'url-loader?limit=8192' },
+            { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff2' },
+            { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff' },
+            { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' }
+        ]
+    }
+};
