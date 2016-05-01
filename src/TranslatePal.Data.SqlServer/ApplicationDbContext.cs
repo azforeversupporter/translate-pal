e43b749b8af1a1eb073f3b45d3cea150a11d1f3c
azforeversupporter <azforeversupporter@gmail.com>
2016-05-01 17:50:46 +0200

    Added Bundle entity (WIP)

--- src/TranslatePal.Data.SqlServer/ApplicationDbContext.cs
+++ src/TranslatePal.Data.SqlServer/ApplicationDbContext.cs
@@ -5,10 +5,14 @@ namespace TranslatePal.Data.SqlServer
     public class ApplicationDbContext : DbContext
     {
         public DbSet<Application> Applications { get; set; }
+        public DbSet<Bundle> Bundles { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Bundle>()
+                .HasAlternateKey(bundle => new { bundle.Name, bundle.ApplicationId });
         }
     }
 }
---
+++ src/TranslatePal.Data.SqlServer/Bundle.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TranslatePal.Data.SqlServer
+{
+    public class Bundle
+    {
+        public Bundle()
+        {
+        }
+
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+
+        [Required]
+        public int ApplicationId { get; set; }
+        public Application Application { get; set; }
+    }
+}
