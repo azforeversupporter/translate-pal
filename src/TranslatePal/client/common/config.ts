b293adf7339a48a483c90158783dba51db610d52
azforeversupporter <azforeversupporter@gmail.com>
2016-06-26 02:43:12 +0200

    Removed previous code

--- src/TranslatePal/client/common/config.ts
+++
@@ -1,5 +0,0 @@
-﻿export default {
-    apiBaseUrl: 'http://localhost:5000/api/v1',
-    authUrl: 'http://localhost:5000/connect/token',
-    tokenName: 'ah12h3'
-}
--- src/TranslatePal/copy-typings.js
+++
@@ -1,8 +0,0 @@
-﻿var mk = require('safe-mkdir');
-var gc = require('glob-copy');
-
-mk.mkdir('./typings/modules');
-gc('./node_modules/aurelia-*/dist/system/*.d.ts', './typings/modules', function (err, files) {
-
-    console.log('Copied ' + files.length + ' files to ./typings/modules');
-});
