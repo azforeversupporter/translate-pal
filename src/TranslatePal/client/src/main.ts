9aaff8daba86b8618bdce5687488144cb3bb9042
azforeversupporter <azforeversupporter@gmail.com>
2016-07-29 01:08:29 +0200

    First steps to Aurelia 1.0

--- src/TranslatePal/client/src/main.ts
+++ src/TranslatePal/client/src/main.ts
@@ -1,4 +1,39 @@
 import {Aurelia} from 'aurelia-framework';
+import {ConfigBuilder} from 'aurelia-materialize-bridge';
+
+import 'materialize-css/bin/materialize.css';
+import 'material-design-icons/iconfont/material-icons.css';
+
+// comment out if you don't want a Promise polyfill (remove also from webpack.config.js)
+import * as Bluebird from 'bluebird';
+Bluebird.config({ warnings: false });
+
+export async function configure(aurelia: Aurelia) {
+
+    aurelia.use
+        .standardConfiguration()
+        .developmentLogging()
+        .plugin('aurelia-materialize-bridge', (config: ConfigBuilder) => config.useAll());
+
+    // Uncomment the line below to enable animation.
+    // aurelia.use.plugin('aurelia-animator-css');
+    // if the css animator is enabled, add swap-order="after" to all router-view elements
+
+    // Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+    // aurelia.use.plugin('aurelia-html-import-template-loader');
+
+    await aurelia.start();
+    aurelia.setRoot('login/login');
+
+    // if you would like your website to work offline (Service Worker), 
+    // install and enable the @easy-webpack/config-offline package in webpack.config.js and uncomment the following code:
+    /*
+    const offline = await System.import('offline-plugin/runtime');
+    offline.install();
+    */
+}
+
+/*import {Aurelia} from 'aurelia-framework';
 import {bootstrap} from 'aurelia-bootstrapper-webpack';
 
 import authConfig from './config/auth-config';
@@ -38,8 +73,6 @@ bootstrap(async (aurelia: Aurelia) => {
 
     // if you would like your website to work offline (Service Worker), 
     // install and enable the @easy-webpack/config-offline package in webpack.config.js and uncomment the following code:
-    /*
-    const offline = await System.import('offline-plugin/runtime');
-    offline.install();
-    */
-});
+    // const offline = await System.import('offline-plugin/runtime');
+    // offline.install();
+});*/
